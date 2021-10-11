725dee20a4ab0d6a560675f1fe7552962c9b3157
Tim Halley <timothy.halley@gmail.com>
2021-10-11 15:20:55 -0700

    OCT11 - Fixed getTodos back to JSON

--- getTodos/index.js
+++ getTodos/index.js
@@ -1,35 +1,14 @@
 const axios = require('axios');
-const cowsay = require('cowsay2');
-const { cat, whale } = require('cowsay2/cows');
-// const seahorse = require('cowsay2/cows/seahorse');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP post request with Axios module.');
 
-    const todoList = await getTodos();
+    const jsonTodos = await getTodos();
     // context.log('here is the data total', todoList)
 
-    var cowTodos = todoList.data;
-    var myList = [];
-    cowTodos.forEach(todo => {
-        if (todo.completed) {
-            myList.push(cowsay.say(todo.title, { cow: cat }));
-        } else {
-            myList.push(cowsay.say(todo.title, { cow: whale }));
-        }
-    });
-
-    var todoHtml = null;
-    myList.forEach(item => {
-
-        todoHtml = todoHtml + item + "\n"
-
-    });
-
-    // console.log(myList)
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: todoHtml
+        body: jsonTodos.data
     };
 
 }
