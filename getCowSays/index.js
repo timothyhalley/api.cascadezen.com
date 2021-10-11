995de52aea3344c389e536abe166e5248c03f907
Tim Halley <timothy.halley@gmail.com>
2021-10-11 14:52:11 -0700

    OCT11 - Save by GitHub again! Reload latest V1

--- getCowSays/index.js
+++ getCowSays/index.js
@@ -1,17 +1,48 @@
+const axios = require('axios');
 const cowsay = require('cowsay2');
+const { cat, whale } = require('cowsay2/cows');
+// const seahorse = require('cowsay2/cows/seahorse');
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('JavaScript HTTP post request with Axios module.');
 
-    const name = (req.query.name || (req.body && req.body.name));
+    const todoList = await getTodos();
+    // context.log('here is the data total', todoList)
 
-    const sayThis = cowsay.say('Hello, you did it ' + name + ', you made your first function!');
-    const sayThat = cowsay.say('Hello too shy! You made your first function!')
-    const responseMessage = name
-        ? sayThis
-        : sayThat
+    var cowTodos = todoList.data;
+    var myList = [];
+    cowTodos.forEach(todo => {
+        if (todo.completed) {
+            myList.push(cowsay.say(todo.title, { cow: cat }));
+        } else {
+            myList.push(cowsay.say(todo.title, { cow: whale }));
+        }
+    });
 
+    var todoHtml = null;
+    myList.forEach(item => {
+
+        todoHtml = todoHtml + item + "\n"
+
+    });
+
+    // console.log(myList)
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: todoHtml
     };
+
+}
+
+async function getTodos() {
+
+    try {
+        const todos = await axios.get('https://jsonplaceholder.typicode.com/todos')
+        //                                                                                                                                               console.log(todos);
+        return todos; // or return a custom object using properties from response
+    } catch (error) {
+        // If the promise rejects, an error will be thrown and caught here
+        context.error(error);
+    }
+
 }
